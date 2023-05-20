fd69086c691831ed123eb07fc338f494451b865b
pheal <atayikilmaz>
2023-05-20 17:20:56 +0300

    form ayarları yapıldı v13

--- src/Form.js
+++ src/Form.js
@@ -14,7 +14,7 @@ export default function Example() {
         <Typography color="gray" className="mt-2 font-normal text-center">
           Bize göndermek istediğiniz argüman bilgilerini giriniz.
         </Typography>
-        <form className="mt-8 mb-2 max-w-screen-lg sm:w-96" action='/' name="oneri" method="POST">
+        <form className="mt-8 mb-2 max-w-screen-lg sm:w-96" action='/home' name="oneri" method="POST">
         <input type="hidden" name="form-name" value="oneri" />         
          <div className="mb-4 flex flex-col gap-6">
             <Input
