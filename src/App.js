869cc64d3c128ff2c05f00be93b5c372bf189969
pheal <atayikilmaz>
2023-05-20 18:11:02 +0300

    form ayarları yapıldı v14

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import Search from './Search';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ArgList from './ArgList';
 import logo from './ArgSozluk.svg';
 import Form from './Form';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import Success from './Success';
 
 function App() {
-
   const scrollToBottom = () => {
     window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
   };
 
-  return <div>
+  return (
+    <Router>
+      <div>
+        <div className="flex justify-between mt-4 h-16">
+          <img src={logo} alt="ArgSozluk" className="inline-block ml-6" style={{ maxWidth: "15rem" }} />
+          <button className="ml-auto mr-6 my-3 rounded-xl hover:bg-gray-200 hover:rounded-lg px-2 border-solid border-4 border-gray-900" onClick={scrollToBottom}>
+            Biz Kimiz?
+          </button>
+        </div>
 
+        <Routes>
+          <Route path="/" element={<ArgList />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
 
-    <div className="flex justify-between mt-4 h-16">
-      <img src={logo} alt="ArgSozluk" className=" inline-block ml-6" style={{ maxWidth: "15rem" }} />
-      <button className="ml-auto mr-6 my-3 rounded-xl hover:bg-gray-200 hover:rounded-lg px-2 border-solid border-4 border-gray-900" onClick={scrollToBottom}>
-        Biz Kimiz?
-      </button>
-    </div>
-
-    <ArgList />
-
-
-    {/* <h2 className="pt-40 pb-10 sticky top-[100vh] text-center text-2xl font-bold">İletişim ve Öneri İçin: netfysoft@netfysoft.com</h2> */}
-
-    <Form />
-
-
-  </div>
-
-
+        {/* <h2 className="pt-40 pb-10 sticky top-[100vh] text-center text-2xl font-bold">İletişim ve Öneri İçin: netfysoft@netfysoft.com</h2> */}
+        
+        <Form />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
