1fd8a2374c486d9fb7e3fe41ae1380a201891351
pheal <atayikilmaz>
2023-05-20 17:04:51 +0300

    form ayarları yapıldı v11

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, Input, Button, Typography, Textarea } from '@material-tailwind/react';
 
 export default function Example() {
- 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Your form submission logic here
 
+    // Show success message
+    alert('Form submitted successfully');
+
+    // Reset form fields
+    event.target.reset();
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -14,35 +22,17 @@ export default function Example() {
         <Typography color="gray" className="mt-2 font-normal text-center">
           Bize göndermek istediğiniz argüman bilgilerini giriniz.
         </Typography>
-        <form className="mt-8 mb-2 max-w-screen-lg sm:w-96" action='./app.js' name="oneri" method="POST">
-        <input type="hidden" name="form-name" value="oneri" />         
-         <div className="mb-4 flex flex-col gap-6">
-            <Input
-            required
-              size="lg"
-              label="Argüman Başlığı"
-              name="argumanBasligi"
-            />
-            <Textarea
-            required
-              size="lg"
-              label="Argüman İçeriği"
-              name="argumanIcerigi"
-              rows={4}
-            />
-            <Textarea
-            required
-              size="lg"
-              label="Kaynaklar"
-              name="kaynaklar"
-            />
+        <form className="mt-8 mb-2 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
+          <input type="hidden" name="form-name" value="oneri" />
+          <div className="mb-4 flex flex-col gap-6">
+            <Input required size="lg" label="Argüman Başlığı" name="argumanBasligi" />
+            <Textarea required size="lg" label="Argüman İçeriği" name="argumanIcerigi" rows={4} />
+            <Textarea required size="lg" label="Kaynaklar" name="kaynaklar" />
           </div>
           <Button type="submit" className="mt-6 mb-6" fullWidth>
             Gönder
           </Button>
         </form>
-        
-        
       </Card>
     </div>
   );
