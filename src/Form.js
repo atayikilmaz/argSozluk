92b91db84bf9075c0caceaa5e07ebc67afad58f4
pheal <atayikilmaz>
2023-05-20 19:41:30 +0300

    form final

--- src/Form.js
+++ src/Form.js
@@ -2,78 +2,78 @@ import React, { useState } from 'react';
 import { Card, Input, Button, Typography, Textarea } from '@material-tailwind/react';
 
 function Form() {
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const form = event.target;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.target;
 
-        try {
-            const response = await fetch('/', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams(new FormData(form)).toString()
-            });
+    try {
+      const response = await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(new FormData(form)).toString()
+      });
 
-            if (response.ok) {
-                // Form submission succeeded
-                // Handle success logic here
-                setShowSuccessMessage(true);
-                console.log('Form submitted successfully');
-            } else {
-                // Form submission failed
-                // Handle error logic here
-                console.error('Form submission failed');
-            }
-        } catch (error) {
-            console.error('An error occurred during form submission:', error);
-        }
-    };
+      if (response.ok) {
+        // Form submission succeeded
+        // Handle success logic here
+        setShowSuccessMessage(true);
+        console.log('Form submitted successfully');
+      } else {
+        // Form submission failed
+        // Handle error logic here
+        console.error('Form submission failed');
+      }
+    } catch (error) {
+      console.error('An error occurred during form submission:', error);
+    }
+  };
 
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <Card color="transparent" shadow={false} className="mx-50%">
-                <Typography variant="h4" color="blue-gray" className="text-center">
-                    Argüman Önerisinde Bulun
-                </Typography>
-                <Typography color="gray" className="mt-2 font-normal text-center">
-                    Bize göndermek istediğiniz argüman bilgilerini giriniz.
-                </Typography>
-                <form className="mt-8 mb-2 max-w-screen-lg sm:w-96" name="oneri" method="POST" onSubmit={handleSubmit}>
-                    <input type="hidden" name="form-name" value="oneri" />
-                    <div className="mb-4 flex flex-col gap-6">
-                        <Input
-                            required
-                            size="lg"
-                            label="Argüman Başlığı"
-                            name="argumanBasligi"
-                        />
-                        <Textarea
-                            required
-                            size="lg"
-                            label="Argüman İçeriği"
-                            name="argumanIcerigi"
-                            rows={4}
-                        />
-                        <Textarea
-                            required
-                            size="lg"
-                            label="Kaynaklar"
-                            name="kaynaklar"
-                        />
-                    </div>
-                    <Button type="submit" className="mt-6 mb-6" fullWidth>
-                        Gönder
-                    </Button>
-                </form>
-                {showSuccessMessage && (
-                    <div className="bg-green-100 bg-opacity-75 rounded-md text-green-600 py-2 px-4 text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                        Başarıyla gönderildi
-                    </div>
-                )}
-            </Card>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      {showSuccessMessage && (
+        <div className="bg-green-100 bg-opacity-75 rounded-md text-green-600 py-2 px-4 text-center fixed top-0 left-1/2 transform -translate-x-1/2">
+          Başarıyla gönderildi
         </div>
-    );
+      )}
+      <Card color="transparent" shadow={false} className="mx-50%">
+        <Typography variant="h4" color="blue-gray" className="text-center">
+          Argüman Önerisinde Bulun
+        </Typography>
+        <Typography color="gray" className="mt-2 font-normal text-center">
+          Bize göndermek istediğiniz argüman bilgilerini giriniz.
+        </Typography>
+        <form className="mt-8 mb-2 max-w-screen-lg sm:w-96" name="oneri" method="POST" onSubmit={handleSubmit}>
+          <input type="hidden" name="form-name" value="oneri" />
+          <div className="mb-4 flex flex-col gap-6">
+            <Input
+              required
+              size="lg"
+              label="Argüman Başlığı"
+              name="argumanBasligi"
+            />
+            <Textarea
+              required
+              size="lg"
+              label="Argüman İçeriği"
+              name="argumanIcerigi"
+              rows={4}
+            />
+            <Textarea
+              required
+              size="lg"
+              label="Kaynaklar"
+              name="kaynaklar"
+            />
+          </div>
+          <Button type="submit" className="mt-6 mb-6" fullWidth>
+            Gönder
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default Form;
