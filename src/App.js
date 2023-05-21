f776f2734b5b696c5190c7c3c66d9b413762ea02
pheal <atayikilmaz>
2023-05-21 15:27:41 +0300

    overflow

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   };
 
   return (
-      <div>
+      <div className="overflow-x-hidden">
        <div className="flex flex-col mt-8 sm:flex-row justify-center sm:justify-between items-center h-16">
   <img src={logo} alt="ArgSozluk" className="mt-12 sm:mt-0 inline-block ml-6" style={{ maxWidth: "15rem" }} />
-  <button className="h-12 sm:h-12 mt-8 sm:mt-0 rounded-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 px-2 border-solid border-4 border-gray-900" onClick={scrollToBottom}>
+  <button className="h-12 mr-4 sm:h-12 mt-8 sm:mt-0 rounded-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 px-2 border-solid border-4 border-gray-900" onClick={scrollToBottom}>
     Biz Kimiz?
   </button>
 </div>
