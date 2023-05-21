1ffa947faac6d119dcd0a2748c7722547929fcb9
pheal <atayikilmaz>
2023-05-21 15:41:51 +0300

    logo spacing

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
       <div className="overflow-x-hidden">
        <div className="flex flex-col mt-8 sm:flex-row justify-center sm:justify-between items-center h-16">
-  <img src={logo} alt="ArgSozluk" className="mt-12 sm:ml-6 sm:mt-0 inline-block ml-4" style={{ maxWidth: "15rem" }} />
-  <button className="p-2 mr-4 sm:h-12 mt-8 sm:mt-0 rounded-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 px-2 border-solid border-4 border-gray-900" onClick={scrollToBottom}>
+  <img src={logo} alt="ArgSozluk" className="mt-16 m-4 sm:ml-6 sm:mt-0 inline-block ml-4" style={{ maxWidth: "15rem" }} />
+  <button className="p-2 mr-4 sm:h-12 mt-4 sm:mt-0 rounded-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 px-2 border-solid border-4 border-gray-900" onClick={scrollToBottom}>
     Biz Kimiz?
   </button>
 </div>
